caf215657f76f5009ac10ec962e2a662818cf046
Pasindurandima <167874999+Pasindurandima@users.noreply.github.com>
2025-07-25 09:28:43 +0530

    delete case

--- demo/demo/src/main/java/repository/AmbulanceRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.AmbulanceRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AmbulanceRequestRepository extends JpaRepository<AmbulanceRequest, Long> {
-}
